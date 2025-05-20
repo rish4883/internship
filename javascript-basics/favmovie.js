c03bb878248b37e3741ff700e436540541abdc85
Rishabh S <rishabh4883@gmail.com>
2025-05-20 17:10:23 +0530

    improve readability

--- javascript-basics/favmovie.js
+++ javascript-basics/favmovie.js
@@ -2,12 +2,14 @@ let movies = ['Interstellar', 'Batman Dark Knight', 'One piece', 'Oppenheimer', 
 
 console.log("\nTotal number of movies: " + movies.length);
 console.log("\nMovie at 3rd index: " + movies[3]);
+
 console.log("\nadding new movie...");
 movies.push("batman v superman");
 console.log("\nUpdated List: ");
 movies.forEach((movie) => {
     console.log('- ' + movie);
 })
+
 console.log("\nRemoving last movie...");
 movies.pop()
 console.log("\nUpdated List: ");

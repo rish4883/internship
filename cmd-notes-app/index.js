2e1d580dc775be094c24fadf254a7a6b32b0a5af
Rishabh S <rishabh@navadhiti.com>
2025-05-26 17:58:27 +0530

    create basic 'interaction' elements

---
+++ cmd-notes-app/index.js
@@ -0,0 +1,33 @@
+const os = require('os');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout
+});
+
+console.log('Choose your action:-');
+console.log('1. Add a new note');
+console.log('2. View all Notes');
+console.log('3. Edit a note');
+console.log('4. Delete a note');
+
+rl.question('Enter your option(1/2/3/4): ', (ans) => {
+    switch(ans) {
+        case 1: addNewNote();
+            break;
+        case 2: viewAll();
+            break;
+        case 3: editNote();
+            break;
+        case 4: deleteNote();
+    }
+});
+
+
+console.log('\nSystem details:');
+console.log('OS: ' + os.platform());
+console.log('Free memory available: ' + os.freemem());
+console.log('Total Uptime: ' + os.uptime());
+
+

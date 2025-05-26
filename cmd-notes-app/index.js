87d89c91902732dcb3de1d38810af955f1197102
Rishabh S <rishabh@navadhiti.com>
2025-05-26 20:13:02 +0530

    change

--- cmd-notes-app/index.js
+++ cmd-notes-app/index.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const readline = require('readline');
+const {stdin, stdout} = require('process')
 
 const rl = readline.createInterface({
     input: stdin,
@@ -11,8 +12,9 @@ console.log('1. Add a new note');
 console.log('2. View all Notes');
 console.log('3. Edit a note');
 console.log('4. Delete a note');
+console.log('5. Exit Application')
 
-rl.question('Enter your option(1/2/3/4): ', (ans) => {
+rl.question('Enter your option(1/2/3/4/5): ', (ans) => {
     switch(ans) {
         case 1: addNewNote();
             break;
@@ -21,6 +23,8 @@ rl.question('Enter your option(1/2/3/4): ', (ans) => {
         case 3: editNote();
             break;
         case 4: deleteNote();
+            break;
+        case 5: process.exit()
     }
 });
 
@@ -29,5 +33,10 @@ console.log('\nSystem details:');
 console.log('OS: ' + os.platform());
 console.log('Free memory available: ' + os.freemem());
 console.log('Total Uptime: ' + os.uptime());
+console.log('\n*********************************************\n');
 
 
+function addNewNote() {
+    console.log('');
+    rl.question('')
+}

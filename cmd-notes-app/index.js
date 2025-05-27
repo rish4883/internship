a9e1cd7b800f8d719e8b111ba68ae48f2072fc13
Rishabh S <rishabh@navadhiti.com>
2025-05-27 10:20:25 +0530

    Added new note feature

--- cmd-notes-app/index.js
+++ cmd-notes-app/index.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const fs = require('fs')
 const readline = require('readline');
 const {stdin, stdout} = require('process')
 
@@ -7,26 +8,14 @@ const rl = readline.createInterface({
     output: stdout
 });
 
+const noteFilePath =  `${__dirname}/notes.txt`;
+
 console.log('Choose your action:-');
 console.log('1. Add a new note');
 console.log('2. View all Notes');
 console.log('3. Edit a note');
 console.log('4. Delete a note');
 console.log('5. Exit Application')
-
-rl.question('Enter your option(1/2/3/4/5): ', (ans) => {
-    switch(ans) {
-        case 1: addNewNote();
-            break;
-        case 2: viewAll();
-            break;
-        case 3: editNote();
-            break;
-        case 4: deleteNote();
-            break;
-        case 5: process.exit()
-    }
-});
 
 
 console.log('\nSystem details:');
@@ -36,7 +25,28 @@ console.log('Total Uptime: ' + os.uptime());
 console.log('\n*********************************************\n');
 
 
+rl.question('Enter your option(1/2/3/4/5): ', (ans) => {
+    switch(ans) {
+        case '1': console.log("option 1"); addNewNote();
+            break;
+        case '2': viewAll();
+            break;
+        case '3': editNote();
+            break;
+        case '4': deleteNote();
+            break;
+        case '5': rl.close();
+            break;
+    }
+});
+
+
 function addNewNote() {
-    console.log('');
-    rl.question('')
+    rl.question('Enter Title of the note: ', (title) => {
+        rl.question('Enter the note: ', (note) => {
+            let fullNote = `${title}-${note}\n`;
+            fs.appendFileSync(noteFilePath, fullNote);
+            rl.close();
+        })
+    });    
 }

b115b5d3f44559624fec458dd1fb10705c0f0ce9
Rishabh S <rishabh@navadhiti.com>
2025-05-28 18:47:08 +0530

    node server concepts and assmt

---
+++ shopping-cart/server.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+
+// This is a function to read a file (html) and send it as text to the front end on request
+function serveStaticFile(res, filename) {
+    fs.readFile(path.join(__dirname, 'public', filename), (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+}
+
+
+
+const server = http.createServer((req, res) => {
+    // root url serve the product form
+    if (req.url == '/') {
+        serveStaticFile(res, 'product-form.html');
+    } 
+    // product-view route serve product-display.html page
+    else if (req.url == '/product-view') {
+        serveStaticFile(res, 'product-display.html');
+    }
+
+    
+    // products get request. It will read all contents of json file and send it to the client 
+    else if (req.method == 'GET' && req.url == '/products')  {
+        fs.readFile('products.json', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(data);
+        })
+    }
+
+    // add new entry
+    else if (req.method == 'POST' && req.url == '/add-products') {
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const newProduct = JSON.parse(body);
+
+            fs.readFile('products.json', (err, data) => {
+                const products = JSON.parse(data);
+                products.push(newProduct);
+
+                fs.writeFile('products.json', JSON.stringify(products, null, 2), err => {
+            if (err) {
+              return send500(res);
+            }
+            res.end(JSON.stringify({ message: 'Product added successfully' }));
+            });
+            
+        });
+    })
+}
+
+    // delete
+    else if (req.method == 'DELETE' && req.url.startsWith('/products/')) {
+        const deleteId = req.url.split('/')[2];
+
+        fs.readFile('products.json', (err, data) => {
+            const products = JSON.parse(data);
+            const newProducts = products.filter(p => p.id != deleteId);
+
+            fs.writeFile('products.json', JSON.stringify(newProducts, null, 2), err => {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Product deleted' }));
+            })
+        });
+    }
+});
+
+
+
+server.listen(3000, () => {
+    console.log("server started....");
+    
+});

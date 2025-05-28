1dec5322fa8a6f8956017ed7043e0d7798dcc228
Rishabh S <rishabh@navadhiti.com>
2025-05-28 13:15:35 +0530

    Assignment 1 simple server completed

---
+++ backend-basics/simple-http-server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Welcome to Home Page</h1>');
+    } else if (req.url == '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>About Us</h1>');
+    } else {
+        res.writeHead(404, {'Content-type': 'text/plain'})
+        res.end('404 Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Listening in port 3000');
+    
+})
